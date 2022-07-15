e1d0ca64f6876d656543cd54f9508a21bbf3af47
mhunter02 <bighunterbg@gmail.com>
2022-07-15 10:15:46 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,7 +14,7 @@ public class QueryProcessor {
         } else if (query.contains("name")) {
            return "Mihail";
         } else { // TODO extend the programm here
-            return "Mihail";
+            return "";
         }
     }
 }
